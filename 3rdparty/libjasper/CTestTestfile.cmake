dbcf4c99280f5cb6cf3441d6ab8ff7eddd85bf38
soulgoast <759433926@qq.com>
2018-11-23 07:20:17 +0800

    3rdparty、bin、include
    Signed-off-by: soulgoast <759433926@qq.com>

---
+++ 3rdparty/libjasper/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/opencv-master/3rdparty/libjasper
+# Build directory: /home/opencv-master/build/3rdparty/libjasper
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
